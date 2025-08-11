d556bee30a5f5eaa08e4df28ad111f0451b6f613
BerganouY <yassineberganouu@gmail.com>
2025-08-11 12:56:53 +0100

    Add 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Ensures SSH client uses ~/.ssh/school and disables password authentication
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    IdentityFile ~/.ssh/school',
+  match => '^\s*IdentityFile\s+',
+}
+
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    PasswordAuthentication no',
+  match => '^\s*PasswordAuthentication\s+',
+}
